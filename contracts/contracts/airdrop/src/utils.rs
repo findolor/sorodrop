1815e904f79c1d47b4ea1a82b41f196b42f4c64d
findolor <anakisci@gmail.com>
2024-07-08 11:40:38 +0300

    feat: add more functions to utils

--- contracts/contracts/airdrop/src/utils.rs
+++ contracts/contracts/airdrop/src/utils.rs
@@ -1,6 +1,6 @@
 use soroban_sdk::Env;
 
-use crate::error::ContractError;
+use crate::{error::ContractError, storage};
 
 pub fn check_timestamp_validity(
     env: &Env,
@@ -19,6 +19,35 @@ pub fn check_timestamp_validity(
             return Err(ContractError::InvalidEndTime {});
         }
     }
-
     Ok(())
+}
+
+pub fn is_airdrop_started(env: &Env, current_timestamp: u64) -> Result<bool, ContractError> {
+    if let Some(start_time) = storage::airdrop::get_start_time(&env)? {
+        if current_timestamp < start_time {
+            return Ok(false);
+        }
+    }
+    Ok(true)
+}
+
+pub fn is_airdrop_ended(env: &Env, current_timestamp: u64) -> Result<bool, ContractError> {
+    if let Some(end_time) = storage::airdrop::get_end_time(&env)? {
+        if current_timestamp < end_time {
+            return Ok(false);
+        }
+    }
+    Ok(true)
+}
+
+pub fn get_airdrop_amounts(env: &Env) -> Result<(i128, i128, i128, i128), ContractError> {
+    let total_amount = storage::airdrop::get_amount(&env)?;
+    let total_claimed = storage::claim::get_total_claimed(&env);
+    let admin_claim = storage::claim::get_admin_claim(&env)?;
+    Ok((
+        total_amount,
+        total_claimed,
+        admin_claim,
+        total_amount - total_claimed - admin_claim,
+    ))
 }
