7b858e224bcb5fe19625338f7f0f04f9a276ebd2
findolor <anakisci@gmail.com>
2024-08-05 12:23:20 +0300

    feat: implement basic logger to index file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,36 @@
-import { Elysia } from "elysia";
+import { Elysia } from "elysia"
+import type { AfterResponseHandler } from "elysia"
+import { v4 as uuid } from "uuid"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { logger } from "./libs/logger"
+import { airdropController } from "./controllers/airdrop"
+
+const app = new Elysia()
+  .derive(() => {
+    return {
+      requestId: uuid(),
+    }
+  })
+  .onRequest(() => {})
+  .onAfterResponse((handler: AfterResponseHandler) => {
+    const { requestId } = handler
+    const { method, url, headers } = handler.request
+    const { status } = handler.set
+    logger.info({
+      requestId,
+      method,
+      url,
+      status,
+      headers,
+      params: handler.params || {},
+      query: handler.query || {},
+      body: handler.body || {},
+    })
+  })
+  .get("/health", () => {})
+  .use(airdropController)
+  .listen(3001)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
