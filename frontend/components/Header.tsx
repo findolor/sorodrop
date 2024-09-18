bc123ac605a8bf251325686b706c15e563746d06
findolor <anakisci@gmail.com>
2024-09-18 15:15:11 +0300

    feat: add an open app button to header for access to the application

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,17 +1,23 @@
 import SocialIcon from "@/components/SocialIcon"
+import Link from "next/link"
 
 const Header = () => (
   <header>
-    <div className="flex flex-col sm:flex-row justify-between items-center py-4 sm:py-8 px-4 sm:px-10">
+    <div className="flex flex-col sm:flex-row justify-between items-center py-4 sm:py-6 px-4 sm:px-10">
       <h1 className="text-3xl sm:text-5xl font-medium text-text-50 mb-4 sm:mb-0">
         Sorodrop
       </h1>
-      <div className="flex space-x-4">
+      <div className="flex items-center space-x-4">
         <SocialIcon type="telegram" />
         <SocialIcon type="github" />
+        <Link href="/app" className="ml-4">
+          <button className="bg-accent-500 hover:bg-accent-600 text-white font-bold py-2 px-4 rounded transition duration-300">
+            Open App
+          </button>
+        </Link>
       </div>
     </div>
-    <div className="w-full h-px bg-gradient-to-r from-transparent via-text-300 to-transparent my-4 sm:my-6 mt-0 opacity-50"></div>
+    <div className="w-full h-px bg-gradient-to-r from-transparent via-text-300 to-transparent opacity-50"></div>
   </header>
 )
 
