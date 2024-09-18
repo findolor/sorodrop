1e36d3e5928d16089969fcde5fb9dcfb7c35595e
findolor <anakisci@gmail.com>
2024-09-18 15:04:58 +0300

    feat: create a separate component for Hero section

---
+++ frontend/components/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero = () => (
+  <header className="text-center py-8 sm:py-16 pb-6 sm:pb-10 bg-transparent px-4">
+    <h2 className="text-4xl sm:text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-orange-500/30 via-[#f2eef7] to-purple-500 animate-gradient">
+      Effortless Airdrops, Maximum Reach
+    </h2>
+    <p className="text-lg sm:text-xl mb-6 sm:mb-8 text-text-200">
+      Simplify token and NFT airdrops on Stellar.
+    </p>
+  </header>
+)
+
+export default Hero
